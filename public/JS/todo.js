ef2ef239b5862218b668e158c123a4781c442777
Roel Cabrera <hellaboredguy@Roels-MacBook-Pro.local>
2017-05-31 10:00:04 -0700

    updated html and css with jquery button functioning

--- public/JS/todo.js
+++ public/JS/todo.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
 	console.log("ready!");
 });
+
+//add to toodoo list
+$(function () {
+	$('.btn-primary').on('click', function () {
+		if ($('.description-input').val().length == 0) {
+			console.log("nothing in text-area bruh...")
+		} else {
+			console.log($('.description-input').val())
+			//push information to table
+			//update table
+			//update columns
+			$('.description-input').val('');
+			console.log("ready for next too-doo");
+		};
+	});
+});
