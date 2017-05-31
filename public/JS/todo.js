435dd1e23ac139f7dd5e78fd2723afb8514d46db
Roel Cabrera <hellaboredguy@Roels-MacBook-Pro.local>
2017-05-31 11:14:43 -0700

    updated html css and js

--- public/JS/todo.js
+++ public/JS/todo.js
@@ -2,13 +2,26 @@ $(document).ready(function () {
 	console.log("ready!");
 });
 
-//add to toodoo list
+//add to toodoo list here
+
+
+// on double click
 $(function () {
-	$('.btn-primary').on('click', function () {
+	$('.flex-box').dblclick(function () {
+		$(this).addClass('done');
+		//push update to table
+		console.log("Done with task");
+	});
+});
+
+//clicking the add button
+$(function () {
+	$('.btn-lg').on('click', function () {
 		if ($('.description-input').val().length == 0) {
 			console.log("nothing in text-area bruh...")
 		} else {
-			console.log($('.description-input').val())
+			console.log($('.description-input').val());
+			console.log($('#day option:selected').text());
 			//push information to table
 			//update table
 			//update columns
