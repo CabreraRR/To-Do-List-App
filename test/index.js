b9eb4f7c81a56c14955ad7631bbc2b2d4c649a1e
Roel Cabrera <hellaboredguy@Roels-MacBook-Pro.local>
2017-06-02 16:10:19 -0700

    hotfix

--- test/index.js
+++ test/index.js
@@ -1,44 +1,29 @@
 const expect = require('chai').expect
 const database = require('../database/actions');
-
-
-describe('Select Query', (done) => {
-  // beforeEach(() => {
-  //   database.truncate()
-  // })
-  //
-  // before((done) => {
-  //   this.todo = {
-  //     title: 'more stuff',
-  //     description: 'describe describe',
-  //     due_date: '2017-06-02T07:00:00.000Z',
-  //     completed: true
-  //   }
-  //
-  //   database.create(this.todo)
-  //     .then(results => {
-  //       done()
-  //     })
-  // })
-
-  it('should give us all the todos',  (done) => {
-  //   [{
-  //    id: 2,
-  //    title: 'more stuff',
-  //    description: 'describe describe',
-  //    due_date: 2017-06-02T07:00:00.000Z,
-  //    completed: true },
-    // {
-  //   id: 1,
-  //   title: 'input goes here',
-  //   description: 'descriptioney stuff',
-  //   due_date: 2017-06-01T07:00:00.000Z,
-  //   completed: false }]
-
-    database.getAll()
-      .then(results => {
-        expect(results[0].title).to.eql('more stuff')
-        done()
-      })
-  })
+console.log(database)
+describe('Action Query', (done) => {
+	it("it exists", () => {
+		expect(database.create('stuff')).to.exist
+	})
+	it("Creates to do list item", () => {
+		expect(database.create('hello')).to.equal('new sticky created')
+	})
+	it("Deletes to do list item", () => {
+		var test_data = {
+			id: 2,
+			title: 'more stuff',
+			description: 'describe describe',
+			completed: true
+		}
+		expect(database.delete(test_data.id)).to.equal('sticky has been deleted')
+	})
+	it("Updates to do list item", () => {
+		var test_data = {
+			id: 2,
+			title: 'more stuff',
+			description: 'describe describe',
+			completed: true
+		}
+		expect(database.update("new list item", test_data.id)).to.equal('sticky has been updated')
+	})
 })
